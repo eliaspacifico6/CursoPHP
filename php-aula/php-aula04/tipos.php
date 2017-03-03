7cc6ceaa90e9f73983462ed72c7bbb1b9d1f14b5
eliaspacifico6 <eliaspacifico6@gmail.com>
2017-03-03 13:20:27 -0300

    reorganização

---
+++ php-aula/php-aula04/tipos.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="_css/estilo.css" >
+    <title></title>
+</head>
+<body>
+<div>
+    <?php
+       $n = 21;
+       $no = "Elias";
+       echo "$no tem $n anos!";
+
+
+    ?>
+</div>
+</body>
+</html>
